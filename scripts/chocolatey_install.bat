13236f9b647a39eba78ff1d6e064273907ad69a8
Stephen Liu <stephen.liu@alumni.stanford.edu>
2017-08-29 10:43:32 -0400

    Add chocolatey install script

---
+++ scripts/chocolatey_install.bat
@@ -0,0 +1,51 @@
+echo "Installing core packages"
+
+call choco install ^
+	7zip.install ^
+	ack ^
+	adobereader ^
+	atom ^
+	autohotkey ^
+	beyondcompare ^
+	brackets ^
+	cmder ^
+	cpu-z ^
+	curl ^
+	cygwin ^
+	f.lux ^
+	fiddler4 ^
+	firefox ^
+	foobar2000 ^
+	gimp ^
+	git ^
+	githubforwindows ^
+	google-chrome-x64 ^
+	gow ^
+	imgburn ^
+	jdk8 ^
+	libreoffice-help ^
+	libreoffice ^
+	linqpad4 ^
+	nodejs.install ^
+	notepadplusplus.install ^
+	npm ^
+	paint.net ^
+	rdcman ^
+	ruby ^
+	scriptcs ^
+	sublimetext3 ^
+	sysinternals ^
+	tor-browser ^
+	truecrypt ^
+	utorrent ^
+	vagrant ^
+	vim ^
+	virtualbox ^
+	virtualbox.extensionpack ^
+	vlc ^
+	wireshark ^
+	
+echo "Installing python and associated packages"
+
+call choco install ^
+	python ^
